6c397a927ea613356447398e696252bc36aff0ef
Alif Wibowo <alif.likes.coding@gmail.com>
2024-06-12 07:36:55 +0700

    Setup routes

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
-import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './sections/Header';
+import HomePage from './pages/HomePage';
+import ShopPage from './pages/ShopPage';
+import AboutPage from './pages/AboutPage';
+import CartPage from './pages/CartPage';
+import NotFound from './pages/NotFound';
+import ProductPage from './pages/ProductPage';
+import Footer from './sections/Footer';
+import './index.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/shop/:productId" element={<ProductPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
